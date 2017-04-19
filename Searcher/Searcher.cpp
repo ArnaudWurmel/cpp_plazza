3ed72297cd002c86b956999b1b7922f3d351dc9a
baptiste hector <baptiste.hector@epitech.eu>
2017-04-19 17:35:15 +0200

    check de la compilation mais pas test

--- Searcher/Searcher.cpp
+++ Searcher/Searcher.cpp
@@ -5,7 +5,7 @@
 // Login   <baptiste@epitech.net>
 // 
 // Started on  Tue Apr 18 13:05:38 2017 baptiste
-// Last update Wed Apr 19 13:49:10 2017 baptiste
+// Last update Wed Apr 19 17:11:42 2017 baptiste
 //
 
 #include <regex>
@@ -22,7 +22,7 @@ void	Plazza::Searcher::matchPhoneNumber(std::string str, std::vector<std::string
   std::regex	check_number("(([0][1-9]) ?([0-9 ]{2}) ?([0-9 ]{2}) ?([0-9]{2}) ?([0-9]{2}))");
   std::smatch	number;
 
-  while (std::regex_search(str, number, check_ip))
+  while (std::regex_search(str, number, check_number))
     {
       vec.push_back(number[0]);
       str = number.suffix().str();
@@ -34,7 +34,7 @@ void	Plazza::Searcher::matchEmailAddress(std::string str, std::vector<std::strin
   std::regex	check_mail("([a-zA-Z0-9_.-]+)@((?:[a-zA-Z0-9]+.)+)([a-zA-Z0-9]{1,4})");
   std::smatch	mail;
 
-  while (std::regex_search(str, mail, check_ip))
+  while (std::regex_search(str, mail, check_mail))
     {
       vec.push_back(mail[0]);
       str = mail.suffix().str();
@@ -53,21 +53,21 @@ void	Plazza::Searcher::matchIpAddress(std::string str, std::vector<std::string>&
     }
 }
 
-std::vector<std::string>	Plazza::Searcher::checkInformation(Command::Information inf, std::vector<std::string> file)
+std::vector<std::string>	Plazza::Searcher::checkInformation(Command::Information inf, std::string file)
 {
-  std::ifstream			fichier(file.front.c_str(), ios::in);
+  std::ifstream			fichier(file, std::ios::in);
   std::vector<std::string>	ret;
   std::string			all_file;
   
   if (fichier)
     {
-      all_file = getline(fichier, all_file, '\0');
-      if (inf == Plazza::Command::Inforamtion::PHONE_NUMBER)
-	this.matchPhoneNumber(all_file, ret)
-      else if (inf == Plazza::Command::Inforamtion::EMAIL_ADDRESS)
-	this.matchEmailAddress(all_file, ret);
+      getline(fichier, all_file, '\0');
+      if (inf == Plazza::Command::Information::PHONE_NUMBER)
+	this->matchPhoneNumber(all_file, ret);
+      else if (inf == Plazza::Command::Information::EMAIL_ADDRESS)
+	this->matchEmailAddress(all_file, ret);
       else
-	this.matchIpAddress(all_file, ret);
+	this->matchIpAddress(all_file, ret);
     }
   return (ret);
 }
--- Searcher/Searcher.hh
+++ Searcher/Searcher.hh
@@ -5,13 +5,14 @@
 // Login   <baptiste@epitech.net>
 // 
 // Started on  Tue Apr 18 13:14:19 2017 baptiste
-// Last update Wed Apr 19 13:45:20 2017 baptiste
+// Last update Wed Apr 19 17:10:00 2017 baptiste
 //
 
 #ifndef SEARCHER_HH_
 # define SEARCHER_HH_
 
 #include <vector>
+#include "Command.hh"
 
 namespace	Plazza
 {
@@ -25,7 +26,7 @@ namespace	Plazza
     void	matchPhoneNumber(std::string, std::vector<std::string>&);
     void	matchEmailAddress(std::string, std::vector<std::string>&);
     void	matchIpAddress(std::string, std::vector<std::string>&);
-    std::vector<std::string> checkInformation(Command::Information, std::vector<std::string>);
+    std::vector<std::string> checkInformation(Command::Information, std::string);
   };
 }
 
